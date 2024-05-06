28f52ebb63b5c57980c7a0cf1a7798de4307b96e
koyota79 <koyota798@gmail.com>
2024-05-07 00:52:41 +0900

    이미지 로딩 처리

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -2,39 +2,48 @@
     <div class="event"> 
         <div>
             <p style="font-weight: bold;font-size: 20px;">이벤트</p>
+        </div>  
+        
+        <div v-for="(item, index) in r_eventImages">
+            <div>
+                <img width="350px" class="img-load" :src="item"  @onerror="imageLoadOnError" />
+            </div>
         </div>
-        <div>
-            <img width="350px" src="../assets/images/event/event_1.png" />
-        </div>
-        <div class="margin_top">
-            <img width="350px" src="../assets/images/event/event_2.png" />
-        </div>
-        <div class="margin_top">
-            <img width="350px" src="../assets/images/event/event2.png" />
-        </div>
+
+
+       
+
+
     </div>
 </template>
- 
-<script>
-    export default {
-        data() {
-            return {
-        }
-
-        },
-        created() {
-        console.log("Parent mounted")
-        this.fetchData(this.$route)
-        
-        },    
-        methods: {
-        fetchData (params) {
-            //console.log(11111111 ,params)
-            console.log('history.state: ', history.state)
-        }
-        }
   
-    };
+<script setup>
+    import {ref, onMounted } from 'vue'
+    let r_eventImages    = ref([])
+    r_eventImages.value = [
+        require('@/assets/images/event/event_1.png'),
+        require('@/assets/images/event/event_2.png'),
+        require('@/assets/images/event/event_3.png'),
+    ]
+    // const emit = defineEmits(["toggle-loading"]);
+    // emit('toggle-loading', true);
+    // onMounted(() => {
+    //     const imgNodes = document.getElementsByClassName("img-load");
+    //     for(let item of imgNodes){
+    //         item.addEventListener("load", function() {
+    //             //console.log(this.complete); // true
+    //             emit('toggle-loading', false); 
+    //         });
+    //     }  
+    
+    // })
+
+    function imageLoadOnError(e) {
+        console.log('imageLoadOnError')
+        e.target.src = require('../assets/images/shop/no_image.jpg');
+    }
+
+   // emit('toggle-loading', false);
 </script>
 
 <style scoped>
